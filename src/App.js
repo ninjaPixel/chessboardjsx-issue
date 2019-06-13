5d7ec07904d84f89019d35f134d14dcd58f8ca9e
ninjaPixel <matthew@ninjaPixel.eu>
2019-06-14 00:40:20 +0100

    use chess.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {useState, useRef, useEffect} from 'react';
 import logo from './logo.svg';
 import Chessboard from 'chessboardjsx'
-import Chess from '@ninjapixel/chess';
+import Chess from 'chess.js';
 import _ from 'lodash';
 
 import './App.css';
